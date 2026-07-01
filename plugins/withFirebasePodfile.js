e8fd7ce23cdc368859fe97a27174bf5b87932478
falachabt <bennytenezeu@gmail.com>
2026-07-01 22:23:15 +0200

    fix(ios): add modular_headers to Firebase pods to fix Swift dependency error without useFrameworks

---
+++ plugins/withFirebasePodfile.js
@@ -0,0 +1,34 @@
+const { withDangerousMod } = require('expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function withFirebasePodfile(config) {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const podfile = path.join(config.modRequest.platformProjectRoot, 'Podfile');
+      let contents = fs.readFileSync(podfile, 'utf8');
+
+      // Add :modular_headers => true to the pods causing the Swift error
+      const podsToPatch = [
+        "pod 'FirebaseAuthInterop', :modular_headers => true",
+        "pod 'FirebaseAppCheckInterop', :modular_headers => true",
+        "pod 'GoogleUtilities', :modular_headers => true",
+        "pod 'RecaptchaInterop', :modular_headers => true"
+      ];
+
+      const patchString = podsToPatch.join('\n  ');
+
+      if (!contents.includes('FirebaseAuthInterop')) {
+        // Insert inside the target block, right before use_react_native!
+        contents = contents.replace(
+          /use_react_native!\(/g,
+          `${patchString}\n  use_react_native!(`
+        );
+        fs.writeFileSync(podfile, contents);
+      }
+      
+      return config;
+    },
+  ]);
+};
